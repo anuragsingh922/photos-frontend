ae34ef4dfa697fbeb844ebec73d9907a0d71f367
sudhanshu105 <85475526+sudhanshu105@users.noreply.github.com>
2024-02-14 14:01:35 +0530

    updated  version 2

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 import Alert from './Components/Alert';
 import Login from './Components/Login';
 import Signup from './Components/Signup';
+import Home from './Components/Home';
 
 function App() {
 
@@ -30,7 +31,7 @@ function App() {
     <>
     <Router>
       <Routes>
-        <Route exact  path="/" element={<><Navbar/><Main showalert={showalert}/></>} />
+        <Route exact  path="/" element={<><Navbar/><Home/></>} />
         <Route exact  path="/user" element={<><UserNavbar/><Main showalert={showalert}/></>} />
         <Route exact  path="/login" element={<><Alert alert={alert}/><Login showalert= {showalert}/></>} />
       <Route exact  path="/signup" element={<><Alert alert={alert}/><Signup showalert={showalert}/></>} />
